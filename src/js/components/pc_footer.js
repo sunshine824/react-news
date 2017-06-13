4fb804b3189066c27dd3521fd694fc07fc208bc8
sunshine824 <cx790168710@aliyun.com>
2017-06-13 16:22:57 +0800

    ok

---
+++ src/js/components/pc_footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Row, Col} from 'antd';
+
+export default class PCFooter extends React.Component {
+    render() {
+        return (
+            <footer>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={20} class="footer">
+                        &copy;&nbsp;2017 ReactNews. All Right Reserved.
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+            </footer>
+        );
+    };
+}
--- src/js/root.js
+++ src/js/root.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router,Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-const history = createBrowserHistory()
-
-import PCIndex from './components/pc_index'
-
+const history = createBrowserHistory();
+import MediaQuery from 'react-responsive';
+import PCIndex from './components/pc_index';
+import MobileIndex from './components/mobile_index'
 
 import 'antd/dist/antd.css';
 
-export default class Root extends React.Component{
-  render(){
-    return (
-      //这里替换了之前的 Index，变成了程序的入口
-      <div>
-        <PCIndex/>
-      </div>
-    );
-  };
+export default class Root extends React.Component {
+    render() {
+        return (
+            //这里替换了之前的 Index，变成了程序的入口
+            <div>
+                <MediaQuery query='(min-device-width: 1224px)'>
+                    <PCIndex/>
+                </MediaQuery>
+                <MediaQuery query='(max-device-width: 1224px)'>
+                    <MobileIndex/>
+                </MediaQuery>
+            </div>
+        );
+    };
 }
 
 ReactDOM.render(<Root/>, document.getElementById('app'));
