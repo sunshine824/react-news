8f114d141484af427db1637f70a7c22d2288b8be
sunshine824 <cx790168710@aliyun.com>
2017-06-15 16:39:45 +0800

    ok

--- src/js/components/pc_news_img_block.js
+++ src/js/components/pc_news_img_block.js
@@ -1,11 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Card} from 'antd'
+import {Link} from 'react-router-dom'
 
-export default class PCIndex extends React.Component {
+export default class PCNewsImageBlock extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            news: ''
+        };
+    }
+
+    componentWillMount() {
+        var myFetchOptions = {
+            method: 'GET'
+        }
+        fetch('http://wangyi.butterfly.mopaasapp.com/news/api?type=' + this.props.type + '&page=' + this.props.page + '&limit=' + this.props.count, myFetchOptions)
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                //console.log(json)
+                this.setState({news: json})
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }
+
     render() {
+        const styleImage={
+            display:"block",
+            width:this.props.imageWidth,
+            height:"90px"
+        }
+        const styleH3={
+            width:this.props.imageWidth,
+            whiteSpace:"nowrap",
+            overflow:'hidden',
+            textOverflow:"ellipsis"
+        }
+        const {news} = this.state;
+        const newsList = news.size
+            ?
+            news.list.map((newItem, index) => {
+                //console.log(newItem)
+                return <div key={index} class="imageblock">
+                            <a href={`details/${newItem.id}`} target="_blank">
+                                <div class="custom-image">
+                                    <img alt="" src={newItem.imgurl} style={styleImage}/>
+                                </div>
+                                <div class="custom-card">
+                                    <h3 style={styleH3}>{newItem.title}</h3>
+                                </div>
+                            </a>
+                        </div>
+            })
+            :
+            '没有加载到数据';
         return (
-            <div>
-
+            <div class="topNewsList">
+                <Card title={this.props.cartTitle} bordered={true} style={{width: this.props.width}}>
+                    {newsList}
+                </Card>
             </div>
         );
     };
