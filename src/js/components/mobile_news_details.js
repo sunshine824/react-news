e5b37d3b42b12d095136afaf9849c69d17d65fff
sunshine824 <cx790168710@aliyun.com>
2017-06-19 10:46:25 +0800

    ok

---
+++ src/js/components/mobile_news_details.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Row, Col, BackTop} from 'antd'
+import MobileHeader from './mobile_header'
+import MobileFooter from './mobile_footer';
+
+export default class PCNewsDetails extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            newsItem: ''
+        };
+    };
+
+    componentDidMount() {
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        const {match} = this.props
+        fetch('http://wangyi.butterfly.mopaasapp.com/detail/api?simpleId=' + match.params.id, myFetchOptions)
+            .then(res => {
+                return res.json()
+            })
+            .then(json => {
+                this.setState({newsItem: json})
+                document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台"
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }
+
+
+    createMarkup() {
+        return {__html: this.state.newsItem.content};
+    };
+
+    render() {
+        const self = this
+        return (
+            <div>
+                <PCHeader/>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={14} class="container">
+                        <div class="articleContainer" dangerouslySetInnerHTML={self.createMarkup()}></div>
+                    </Col>
+                    <Col span={6}>
+                        <PCNewsImageBlock type="war" count={10} page={1}  width="100%" cartTitle="军事新闻" imageWidth="150px" />
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+                <PCFooter/>
+                <BackTop/>
+            </div>
+        );
+    };
+}
